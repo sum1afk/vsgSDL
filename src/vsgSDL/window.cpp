51d6c91b5ef931733b1d75a29475afe13e313d2a
Ashley Rogers <ashley@anime.lgbt>
2024-04-25 21:25:46 -0400

    Update vsg, use vcpkg

--- src/vsgSDL/window.cpp
+++ src/vsgSDL/window.cpp
@@ -1,13 +1,9 @@
 #include <vsgSDL/window.hpp>
 
-#include <vsg/viewer/Viewer.h>
-#include <vsg/viewer/WindowAdapter.h>
-#include <vsg/ui/ScrollWheelEvent.h>
+#include <vsg/all.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
-
-#include<vsg/all.h>
 
 namespace vsgSDL {
     static constexpr auto instanceSurfaceName = []() constexpr {
@@ -26,14 +22,26 @@ namespace vsgSDL {
     Window::Window() {
         keyboardMap = KeyboardMap::create();
         traits = vsg::WindowTraits::create();
+		oldKeyStateSDL = nullptr;
+		sdlWindow = nullptr;
     }
 
     Window::~Window() {
-        renderDeinit(viewer);
+		if(viewer.valid())
+		{
+        	renderDeinit(viewer);
+		}
 
-        delete [] oldKeyStateSDL;
+		if(oldKeyStateSDL != nullptr)
+		{
+        	delete[] oldKeyStateSDL;
+		}
 
-        SDL_DestroyWindow(sdlWindow);
+		if(sdlWindow != nullptr)
+		{
+        	SDL_DestroyWindow(sdlWindow);
+		}
+
         SDL_Quit();
         SDL_Vulkan_UnloadLibrary();
     }
@@ -148,10 +156,10 @@ namespace vsgSDL {
             return false;
 
         if(!initSDL())
-            return false;
+			return false;
 
         if(!initSDLVulkan(vulkanDynLib))
-            return false;
+			return false;
 
         traits->x = x;
         traits->y = y;
